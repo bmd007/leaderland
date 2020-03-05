acc6af3a536bdc6b4d120b9f9221d2116c293e7f
bmd007 <bmd579@gmail.com>
2020-03-05 16:31:01 +0100

    foundation

---
+++ src/main/java/leaderland/leaderland/LeaderlandApplication.java
@@ -0,0 +1,13 @@
+package leaderland.leaderland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderlandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leaderland/leaderland/LeaderlandApplicationTests.java
@@ -0,0 +1,13 @@
+package leaderland.leaderland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
